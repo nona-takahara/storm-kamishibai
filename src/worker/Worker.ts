d52305b57c697d1d3103c0ba49670c299bfd961a
Nona Takahara <45140117+nona-takahara@users.noreply.github.com>
2022-08-30 04:34:47 +0900

    WIP: Nested Worker非対応ブラウザ処理方針確定

--- src/worker/Worker.ts
+++ src/worker/Worker.ts
@@ -5,25 +5,46 @@ import WorkerCommand from "./WorkerCommand";
 export default {}
 
 // eslint-disable-next-line
-const ctx: Worker = self as any;
+const ctx: any = self as any;
 let workerData: any;
 
 ctx.addEventListener('message', (evt: MessageEvent<WorkerCommand>) => {
   const data = evt.data;
+  if (!workerData.subWorker) {
+    if (ctx.Worker) {
+      workerData.subWorker = new Worker(new URL('../gencode/GenCode.ts', import.meta.url));
+    } else {
+      workerData.subWorker = ctx;
+    }
+  }
+
   if (data instanceof OpenFileCommand) {
     const rdata = new Uint32Array(data.u8Image.buffer); // uint32による生データ
-    const cs32 = Uint32Array.from(new Set(rdata)).reverse(); // デフォルトのパレット順序
+    const cs32 = Uint32Array.from(new Set(rdata)).reverse(); // 基準パレット生成
+
+    // コマンドを生成し、postする
+
+    workerData.rdata = rdata;
 
     // Appにはuint32のパレットデータのコピーのみ返却し、App側でUI処理を実施
     // Appからuint32のパレットデータを再度受け取り、インデックスで処理する
 
     // Worker <-> App：元画像の色
-    // Appでのみ変換先の色を知っている状態にしてしまう
+    // Appでのみ変換後にLuaで使う色を知っている状態にしてしまう
   }
   
-  // SafariくんがNested Workerに対応してくれないので、yieldっぽい感じで実装する
-  // 1. Apps側から最初にパレット情報を送信する
-  // 2. Apps側から今回の切り抜き範囲を送信し、パレット1枚分進める
-  // 主導権を常にApps側に握らせ、progress barもAppsで管理させる
-  // 代わりに、
+  // Nested Workerに対応かどうかによって.subWorkerの指すWorkerが変わる！
+  //
+  // 1. Apps -> Worker (start-convert)
+  // 2. Worker -> SubWorker (convert-card*)
+  // 3. SubWorker -> Worker (convert-succeed*)
+  // 4. Worker -> Apps (convert-result)
+  // 5. 2に戻る(実際は先にconvert-cardを発行し、続けてconvert-resultを処理・発行する)
+  // 6. Worker -> Apps (convert-end)
+  // !. Apps -> Worker (convert-terminate!)
+  // 
+  // *がついているコマンドは、単純に.subWorkerにコマンドを渡せばよい。
+  // Appsはコマンド種別を見て、ただお隣に横流しするだけで終わる。
+  // terminateコマンドは、Nested Worker対応ならば直接SubWorkerを止めるが、
+  // 非対応ならAppsにterminateコマンドを送り付ける必要がある。
 });
