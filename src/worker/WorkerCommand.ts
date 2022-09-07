19912bcc02789ed9abf0450c97228a985d5b6385
Nona Takahara <45140117+nona-takahara@users.noreply.github.com>
2022-09-07 10:23:42 +0900

    Revert "Merge branch 'logic-rewrite' of https://github.com/nona-takahara/storm-kamishibai into logic-rewrite"
    This reverts commit 2d99c1ec87a62888b0c633d937c856e5200c56c5, reversing
    changes made to 423ae267114bf2bd4f1f852df553d8efadff8b7b.

--- src/worker/WorkerCommand.ts
+++ src/worker/WorkerCommand.ts
@@ -1,16 +1,4 @@
-import ConvertResultCommand from "./ConvertResultCommand";
-import ConvertSucceedCommand from "./ConvertSucceedCommand";
-import FileLoadedCommand from "./FileLoadedCommand";
-import OpenFileCommand from "./OpenFileCommand";
-import StartConvertCommand from "./StartConvertCommand";
-import TerminateConverterCommand from "./TerminateConverterCommand";
-import ConvertCardCommand from "./ConvertCardCommand";
-import EndConvertCommand from "./EndConvertCommand";
-
 export default abstract class WorkerCommand {
   readonly abstract command: string;
   public abstract getTransfer(): Array<Transferable>;
 }
-
-export { ConvertResultCommand, ConvertSucceedCommand, FileLoadedCommand, OpenFileCommand, 
-StartConvertCommand, TerminateConverterCommand, ConvertCardCommand, EndConvertCommand }
