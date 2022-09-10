b8aede74c65428f8a14df87c122bc260832faf29
Nona Takahara <45140117+nona-takahara@users.noreply.github.com>
2022-09-10 23:16:33 +0900

    as={Col}を使用してdivを節約

--- src/ui/MainSettingTab.tsx
+++ src/ui/MainSettingTab.tsx
@@ -87,70 +87,64 @@ export default class MainSettingTab extends React.Component<MainSettingTabProps>
                 <Form.Label column xs={2}>
                   サイズ
                 </Form.Label>
-                <Col>
-                  <InputGroup>
-                    <LabeledInput
-                      label="幅"
-                      type="number"
-                      defaultValue={this.props.convertOption.luaCardWidth}
-                      onChange={(e) => {
-                        this.props.changeConvertSettings({ luaCardWidth: Number(e.target.value) }, true);
-                      }} />
-                    <LabeledInput
-                      label="高さ"
-                      type="number"
-                      defaultValue={this.props.convertOption.luaCardHeight}
-                      onChange={(e) => {
-                        this.props.changeConvertSettings({ luaCardHeight: Number(e.target.value) }, true);
-                      }} />
-                  </InputGroup>
-                </Col>
+                <InputGroup as={Col}>
+                  <LabeledInput
+                    label="幅"
+                    type="number"
+                    defaultValue={this.props.convertOption.luaCardWidth}
+                    onChange={(e) => {
+                      this.props.changeConvertSettings({ luaCardWidth: Number(e.target.value) }, true);
+                    }} />
+                  <LabeledInput
+                    label="高さ"
+                    type="number"
+                    defaultValue={this.props.convertOption.luaCardHeight}
+                    onChange={(e) => {
+                      this.props.changeConvertSettings({ luaCardHeight: Number(e.target.value) }, true);
+                    }} />
+                </InputGroup>
               </Row>
               <Row className="mt-3">
                 <Form.Label column xs={2}>
                   開始位置
                 </Form.Label>
-                <Col>
-                  <InputGroup>
-                    <LabeledInput
-                      label="X"
-                      type="number"
-                      defaultValue={this.props.convertOption.pictureOffsetX}
-                      onChange={(e) => {
-                        this.props.changeConvertSettings({ pictureOffsetX: Number(e.target.value) }, true);
-                      }} />
-                    <LabeledInput
-                      label="Y"
-                      type="number"
-                      defaultValue={this.props.convertOption.pictureOffsetY}
-                      onChange={(e) => {
-                        this.props.changeConvertSettings({ pictureOffsetY: Number(e.target.value) }, true);
-                      }} />
-                  </InputGroup>
-                </Col>
+                <InputGroup as={Col}>
+                  <LabeledInput
+                    label="X"
+                    type="number"
+                    defaultValue={this.props.convertOption.pictureOffsetX}
+                    onChange={(e) => {
+                      this.props.changeConvertSettings({ pictureOffsetX: Number(e.target.value) }, true);
+                    }} />
+                  <LabeledInput
+                    label="Y"
+                    type="number"
+                    defaultValue={this.props.convertOption.pictureOffsetY}
+                    onChange={(e) => {
+                      this.props.changeConvertSettings({ pictureOffsetY: Number(e.target.value) }, true);
+                    }} />
+                </InputGroup>
               </Row>
               <Row>
                 <Form.Label column xs={2}>
                   スキップ
                 </Form.Label>
-                <Col>
-                  <InputGroup>
-                    <LabeledInput
-                      label="水平"
-                      type="number"
-                      defaultValue={this.props.convertOption.pictureSkipH}
-                      onChange={(e) => {
-                        this.props.changeConvertSettings({ pictureSkipH: Number(e.target.value) }, true);
-                      }} />
-                    <LabeledInput
-                      label="垂直"
-                      type="number"
-                      defaultValue={this.props.convertOption.pictureSkipV}
-                      onChange={(e) => {
-                        this.props.changeConvertSettings({ pictureSkipV: Number(e.target.value) }, true);
-                      }} />
-                  </InputGroup>
-                </Col>
+                <InputGroup as={Col}>
+                  <LabeledInput
+                    label="水平"
+                    type="number"
+                    defaultValue={this.props.convertOption.pictureSkipH}
+                    onChange={(e) => {
+                      this.props.changeConvertSettings({ pictureSkipH: Number(e.target.value) }, true);
+                    }} />
+                  <LabeledInput
+                    label="垂直"
+                    type="number"
+                    defaultValue={this.props.convertOption.pictureSkipV}
+                    onChange={(e) => {
+                      this.props.changeConvertSettings({ pictureSkipV: Number(e.target.value) }, true);
+                    }} />
+                </InputGroup>
               </Row>
             </Stack>
           </Card.Body>
