89648835f799e6006c50e091409f827048192bc2
Nona Takahara <45140117+nona-takahara@users.noreply.github.com>
2022-09-10 23:15:32 +0900

    設定画面の構成変更

--- src/ui/BasicSettingTab.tsx
+++ src/ui/BasicSettingTab.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { Card, InputGroup, Row, Col, FormControl, Stack, Form, Alert, ButtonGroup, ToggleButton} from "react-bootstrap";
+import { Card, InputGroup, Row, Col, FormControl, Stack, Form, Alert, ButtonGroup, ToggleButton } from "react-bootstrap";
 import { BsArrowDownSquareFill, BsArrowLeftSquareFill, BsArrowRightSquareFill, BsArrowUpSquareFill, BsExclamationCircle } from "react-icons/bs";
 import ConvertOption from "../ConvertOption";
 import LuaCodeOption from "../LuaCodeOption";
+import LabeledInput from "./util/LabeledInput";
 
 export type BasicSettingTabProps = {
   changeLuaCodeSettings: (v: LuaCodeOption | any, needReconvert?: boolean) => any;
@@ -37,14 +38,16 @@ export default class BasicSettingTab extends React.Component<BasicSettingTabProp
                 <a href="https://steamcommunity.com/sharedfiles/filedetails/?id=2850204940" target="_blank">Analog Destination Indicator(Rollsign)</a>
                 用の変換結果を出力します。<br />以下の一部設定は無効となります。
               </Card.Text>
-              <InputGroup>
-                <InputGroup.Text>幕間の隙間</InputGroup.Text>
-                <FormControl
-                  type="number"
-                  defaultValue={this.props.luaCodeOption.luaRollSignGap}
-                  disabled={!this.props.luaCodeOption.isRollSign}
-                  onChange={(evt) => { this.props.changeLuaCodeSettings({ luaRollSignGap: Number(evt.target.value) }); }} />
-              </InputGroup>
+              <Form.Group as={Row}>
+                <Form.Label column xs={3}>幕間の隙間</Form.Label>
+                <Col>
+                  <FormControl
+                    type="number"
+                    defaultValue={this.props.luaCodeOption.luaRollSignGap}
+                    disabled={!this.props.luaCodeOption.isRollSign}
+                    onChange={(evt) => { this.props.changeLuaCodeSettings({ luaRollSignGap: Number(evt.target.value) }); }} />
+                </Col>
+              </Form.Group>
               {(this.props.convertOption.luaCardHeight + this.props.luaCodeOption.luaRollSignGap) !== 32 ? (<Alert variant="warning" className="mb-0">
                 <Row>
                   <Col xs="auto" className="pe-0">
@@ -58,51 +61,65 @@ export default class BasicSettingTab extends React.Component<BasicSettingTabProp
             </Stack>
           </Card.Body>
         </Card>
-        <InputGroup>
-          <InputGroup.Text>先頭の画像インデックス</InputGroup.Text>
-          <FormControl
-            type="number"
-            defaultValue={this.props.luaCodeOption.luaCardIndexStartWith}
-            disabled={this.props.luaCodeOption.isRollSign}
-            onChange={(evt) => { this.props.changeLuaCodeSettings({ luaCardIndexStartWith: Number(evt.target.value) }); }} />
-        </InputGroup>
-        <InputGroup>
-          <InputGroup.Text>読み込む数値チャンネル</InputGroup.Text>
-          <FormControl
-            type="number"
-            defaultValue={this.props.luaCodeOption.luaReadChannel}
-            disabled={this.props.luaCodeOption.isRollSign}
-            onChange={(evt) => { this.props.changeLuaCodeSettings({ luaReadChannel: Number(evt.target.value) }); }} />
-        </InputGroup>
-        <InputGroup>
-          <InputGroup.Text>左側<br />オフセット</InputGroup.Text>
-          <FormControl
-            type="number"
-            defaultValue={this.props.luaCodeOption.luaOffsetX}
-            onChange={(evt) => { this.props.changeLuaCodeSettings({ luaOffsetX: Number(evt.target.value) }); }} />
-          <InputGroup.Text>上側<br />オフセット</InputGroup.Text>
-          <FormControl
-            type="number"
-            defaultValue={this.props.luaCodeOption.luaOffsetY}
-            disabled={this.props.luaCodeOption.isRollSign}
-            onChange={(evt) => { this.props.changeLuaCodeSettings({ luaOffsetY: Number(evt.target.value) }); }} />
-        </InputGroup>
-        <InputGroup>
-          <InputGroup.Text>水平倍率</InputGroup.Text>
-          <FormControl
-            type="number"
-            defaultValue={this.props.luaCodeOption.luaScaleH}
-            disabled={this.props.luaCodeOption.isRollSign}
-            onChange={(evt) => { this.props.changeLuaCodeSettings({ luaScaleH: Number(evt.target.value) }); }} />
-          <InputGroup.Text>垂直倍率</InputGroup.Text>
-          <FormControl
-            type="number"
-            defaultValue={this.props.luaCodeOption.luaScaleV}
-            disabled={this.props.luaCodeOption.isRollSign}
-            onChange={(evt) => { this.props.changeLuaCodeSettings({ luaScaleV: Number(evt.target.value) }); }} />
-        </InputGroup>
+        <Form.Group as={Row}>
+          <Form.Label column xs={4}>先頭の画像インデックス</Form.Label>
+          <Col>
+            <Form.Control
+              type="number"
+              defaultValue={this.props.luaCodeOption.luaCardIndexStartWith}
+              disabled={this.props.luaCodeOption.isRollSign}
+              onChange={(evt) => { this.props.changeLuaCodeSettings({ luaCardIndexStartWith: Number(evt.target.value) }); }} />
+          </Col>
+        </Form.Group>
+        <Form.Group as={Row}>
+          <Form.Label column xs={4}>読み込む数値チャンネル</Form.Label>
+          <Col>
+            <Form.Control
+              type="number"
+              defaultValue={this.props.luaCodeOption.luaReadChannel}
+              disabled={this.props.luaCodeOption.isRollSign}
+              onChange={(evt) => { this.props.changeLuaCodeSettings({ luaReadChannel: Number(evt.target.value) }); }} />
+          </Col>
+        </Form.Group>
         <Row>
-          <Col xs="auto">回転</Col>
+          <Form.Label column xs={2}>
+            描画<br />オフセット
+          </Form.Label>
+          <InputGroup as={Col}>
+            <LabeledInput
+              label="左側"
+              type="number"
+              defaultValue={this.props.luaCodeOption.luaOffsetX}
+              onChange={(evt) => { this.props.changeLuaCodeSettings({ luaOffsetX: Number(evt.target.value) }); }} />
+            <LabeledInput
+              label="上側"
+              type="number"
+              defaultValue={this.props.luaCodeOption.luaOffsetY}
+              disabled={this.props.luaCodeOption.isRollSign}
+              onChange={(evt) => { this.props.changeLuaCodeSettings({ luaOffsetY: Number(evt.target.value) }); }} />
+          </InputGroup>
+        </Row>
+        <Row>
+          <Form.Label column xs={2}>
+            描画倍率
+          </Form.Label>
+          <InputGroup as={Col}>
+            <LabeledInput
+              label="水平"
+              type="number"
+              defaultValue={this.props.luaCodeOption.luaScaleH}
+              disabled={this.props.luaCodeOption.isRollSign}
+              onChange={(evt) => { this.props.changeLuaCodeSettings({ luaScaleH: Number(evt.target.value) }); }} />
+            <LabeledInput
+              label="垂直"
+              type="number"
+              defaultValue={this.props.luaCodeOption.luaScaleV}
+              disabled={this.props.luaCodeOption.isRollSign}
+              onChange={(evt) => { this.props.changeLuaCodeSettings({ luaScaleV: Number(evt.target.value) }); }} />
+          </InputGroup>
+        </Row>
+        <Row>
+          <Col xs={2}>回転</Col>
           <Col className="d-grid">
             <ButtonGroup>
               {[
