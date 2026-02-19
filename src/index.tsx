9c5c834f083840ccabf59cd948107224e07045be
Nona Takahara <nona_t_2612@yahoo.co.jp>
2026-02-19 23:08:27 +0900

    フォーマッタ適用漏れを修正

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import App from "./App";
 import { createRoot } from "react-dom/client";
 import "./i18n";
-import "./App.scss"
+import "./App.scss";
 
 createRoot(document.getElementById("root")!).render(<App />);
